e903f992520c0ed298f6631afc2265e55271898f
Sébastien Barbieri <sebastien.barbieri@gmail.com>
2014-03-05 00:00:19 +0100

    karma and protractor running

--- config/protractor-conf.js
+++ config/protractor-conf.js
@@ -9,7 +9,7 @@ exports.config = {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:8000/app/',
+  baseUrl: 'http://github.local.opendojo.org/Opendojo/angularjs-hello-world/app/',
 
   framework: 'jasmine',
 
